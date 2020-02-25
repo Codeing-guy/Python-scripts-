af3e561adf3ee1290e7183675f880a218ec264e8
coderamit <59659828+Codeing-guy@users.noreply.github.com>
2020-02-25 21:25:21 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,9 +1,3 @@
-##from selenium import webdriver
-##
-##driver=webdriver.Chrome()
-##driver.get()
-##driver.close()
-
 
 #code for opening yt if no arguments are passed else open whatsapp or yt with some search content
 import webbrowser ,sys #,pyperclip
@@ -14,9 +8,6 @@ if len(sys.argv) >1 and sys.argv[1]=='wa':
 elif len(sys.argv) >1:
     searchIteam=' '.join(sys.argv[1:])
     webbrowser.open('https://www.youtube.com/results?search_query='+searchIteam)
-##elif len(pyperclip.paste())>0:
-##    searchIteam=pyperclip.paste()
-##    webbrowser.open('https://www.youtube.com/results?search_query='+searchIteam)
 else:
      webbrowser.open('https://www.youtube.com')
 
